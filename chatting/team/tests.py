277137da2f7552661fbd7ba14e872cf5248ce8e6
Bae KwonHan <darjeeling@gmail.com>
2015-10-03 21:36:57 +0900

    add flake8 support

--- chatting/team/tests.py
+++ chatting/team/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 
 # Create your tests here.
 
+
 class TestTestCase(TestCase):
     def test_test1(self):
-        self.assertTrue(True,'true is true')
+        self.assertTrue(True, 'true is true')
