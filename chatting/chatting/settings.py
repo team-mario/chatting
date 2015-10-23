ebb124b6f4d9c9e71a0f3cc6d74b57cd60eda04c
Park <bbayoung7849@gmail.com>
2015-10-23 13:29:19 +0900

    update settings.py - use os.path.join

--- chatting/chatting/settings.py
+++ chatting/chatting/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'chatting.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR + "/templates", ],
+        'DIRS': [os.path.join(BASE_DIR, "templates"), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
