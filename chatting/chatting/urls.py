705cb2afb1b217057c004e6b6e32c1632d07fbbe
Park <bbayoung7849@gmail.com>
2015-10-23 20:52:57 +0900

    add list.html, update FT code and pass

--- chatting/chatting/urls.py
+++ chatting/chatting/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^messages/project-plan$', 'message.views.list'),
     url(r'^', 'team.views.index'),
 ]
--- chatting/functional_tests/tests.py
+++ chatting/functional_tests/tests.py
@@ -10,10 +10,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_new_visitor(self):
-        # execute browser
-        self.browser.get(self.live_server_url)
-
+    def check_basic_layout(self):
         # check browser title
         self.assertIn('issue chat', self.browser.title)
 
@@ -40,3 +37,25 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # check search button
         btn_search = self.browser.find_element_by_id('btn_search')
         self.assertEqual(btn_search.get_attribute('value'), 'Search')
+
+    def test_new_visitor(self):
+        # execute browser
+        self.browser.get(self.live_server_url)
+
+        self.check_basic_layout()
+
+        # find element by id 'project plan' issue
+        issue_project_plan = self.browser.find_element_by_id('project-plan')
+
+        url_regex_str = '/messages/.+'
+
+        # check issue 'project plan' href
+        self.assertRegex(issue_project_plan.get_attribute('href'),
+                         url_regex_str)
+
+        issue_project_plan.click()
+
+        # current browser url validation width regex
+        self.assertRegex(self.browser.current_url, url_regex_str)
+
+        self.check_basic_layout()
