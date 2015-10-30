7e0733f26f2eb9f5719e4b145333895db2346e7c
JudeLee <dongyub63@gmail.com>
2015-10-30 18:39:45 +0900

    Add function limiting access from anynimous user and pass test

--- chatting/team/migrations/0001_initial.py
+++ chatting/team/migrations/0001_initial.py
@@ -2,33 +2,31 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
             name='IssueChannel',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
                 ('channel_name', models.CharField(max_length=30)),
                 ('channel_content', models.CharField(default='', max_length=255)),
+                ('user', models.ForeignKey(default=None, to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
             name='UserInfo',
             fields=[
-                ('user_id', models.CharField(serialize=False, primary_key=True, max_length=20)),
+                ('user_id', models.CharField(primary_key=True, serialize=False, max_length=20)),
                 ('user_password', models.CharField(max_length=30)),
                 ('user_email', models.EmailField(default='', max_length=30)),
             ],
-        ),
-        migrations.AddField(
-            model_name='issuechannel',
-            name='user_id',
-            field=models.ForeignKey(default=None, to='team.UserInfo'),
         ),
     ]
