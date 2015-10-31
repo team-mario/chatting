6d1d4ad174b3c51a71180ee79e9aa448cd970615
JudeLee <dongyub63@gmail.com>
2015-10-31 21:54:42 +0900

    Add hash tag model as many-to-many relationsships with IssueChannel Model

--- chatting/team/models.py
+++ chatting/team/models.py
@@ -2,13 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class UserInfo(models.Model):
-    user_id = models.CharField(max_length=20, primary_key=True)
-    user_password = models.CharField(max_length=30)
-    user_email = models.EmailField(max_length=30, default='')
-
-
 class IssueChannel(models.Model):
     user = models.ForeignKey(User, default=None)
     channel_name = models.CharField(max_length=30)
     channel_content = models.CharField(max_length=255, default='')
+
+
+class HashTag(models.Model):
+    channels = models.ManyToManyField(IssueChannel)
+    tag_name = models.CharField(max_length=20)
---
+++ chatting/team/tests/test_hash_tag.py
@@ -0,0 +1,8 @@
+__author__ = 'judelee'
+from django.test import TestCase
+from team.models import IssueChannel
+from django.contrib.auth.models import User
+
+
+class HashTagTest(TestCase):
+    pass
