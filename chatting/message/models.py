56d0c3d5d8f15173f76c2b09ac9a6417b60d0229
JudeLee <dongyub63@gmail.com>
2015-11-02 16:21:50 +0900

    Resolve conflicts

--- chatting/message/models.py
+++ chatting/message/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from team.models import IssueChannel
 
 
 # Create your models here.
 class Message(models.Model):
     sender = models.CharField(max_length=255, default=None)
-    datetime = models.DateTimeField(auto_now_add=True)
     content = models.TextField(default=None)
-    channel = models.ForeignKey(IssueChannel, default=None)
+    # channel = models.ForeignKey(IssueChannel, default=None)
+    create_datetime = models.DateTimeField(auto_now_add=True)
