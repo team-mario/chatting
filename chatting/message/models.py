49efdc757312bbaa39fd7a886c7b933615fee7cd
JudeLee <dongyub63@gmail.com>
2015-11-02 16:43:05 +0900

    Reslove conflicts

--- chatting/message/models.py
+++ chatting/message/models.py
@@ -5,5 +5,4 @@ from django.db import models
 class Message(models.Model):
     sender = models.CharField(max_length=255, default=None)
     content = models.TextField(default=None)
-    # channel = models.ForeignKey(IssueChannel, default=None)
     create_datetime = models.DateTimeField(auto_now_add=True)
