0e94565a668696726cd5d9fbbe14f884c03f0729
Park <bbayoung7849@gmail.com>
2015-11-07 21:28:54 +0900

    change 'sender' to User in Message model

--- chatting/message/models.py
+++ chatting/message/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from team.models import Issue
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Message(models.Model):
     issue = models.ForeignKey(Issue, default=None)
-    sender = models.CharField(max_length=255, default=None)
+    user = models.ForeignKey(User, default=None)
     content = models.TextField(default=None)
     create_datetime = models.DateTimeField(auto_now_add=True)
