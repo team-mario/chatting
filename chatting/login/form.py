9d41e11e54f8ae03afc416e69320da8b63c21f53
Park <bbayoung7849@gmail.com>
2015-11-14 00:28:57 +0900

    registration view css complete

--- chatting/login/form.py
+++ chatting/login/form.py
@@ -4,34 +4,38 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class RegistrationForm(forms.Form):
-
     username = forms.RegexField(regex=r'^\w+$',
                                 widget=forms.TextInput(attrs=dict(
                                     required=True,
-                                    max_length=30)),
-                                label=_("Username"),
+                                    max_length=30,
+                                    placeholder='Enter your name')),
+                                label=_("Enter your name"),
                                 error_messages={'invalid': ("This value must"
                                                             "contain "
                                                             "only letters,"
                                                             "numbers and"
                                                             "underscores.")})
+
     email = forms.EmailField(widget=forms.TextInput(
         attrs=dict(required=True,
-                   max_length=30)),
-        label=_("Email address"))
+                   max_length=30,
+                   placeholder='Enter your email')),
+        label=_("Enter your email"))
 
     password1 = forms.CharField(widget=forms.PasswordInput(
         attrs=dict(
             required=True,
             max_length=30,
-            render_value=False)),
-        label=_("Password"))
+            render_value=False,
+            placeholder='Enter your password')),
+        label=_("Enter your password"))
 
     password2 = forms.CharField(widget=forms.PasswordInput(
         attrs=dict(required=True,
                    max_length=30,
-                   render_value=False)),
-        label=_("Password (again)"))
+                   render_value=False,
+                   placeholder='Enter your password again')),
+        label=_("Enter your password again"))
 
     def clean_username(self):
         try:
@@ -46,5 +50,5 @@ class RegistrationForm(forms.Form):
         if 'password1' in cleaned_value and 'password2' in cleaned_value:
             if cleaned_value['password1'] != cleaned_value['password2']:
                 raise forms.ValidationError(_("The two password fields"
-                                              "did not match."))
+                                              " did not match."))
         return cleaned_value
