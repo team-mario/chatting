c5a18ed9ba83df8cf949adc98f3876a16f47926f
Park <bbayoung7849@gmail.com>
2015-11-13 15:07:35 +0900

    layout bug fix in message issues

--- chatting/login/tests/tests.py
+++ chatting/login/tests/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
-from login.views import index 
+from login.views import index
 from django.core.urlresolvers import resolve
+
 
 class LoginTest(TestCase):
     def test_root_url_resolves_to_index(self):
